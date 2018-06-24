59a11dda64054f8d566a0365089f17d9622dfaf9
Thorley <thorley.eric@gmail.com>
2018-06-24 13:05:32 -0400

    psuedo code JS

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//Initial array//
+
+//creating the buttons based on the array//  
+
+// add the button based on the array on the search //
+
+// the AJAX call //
+
+// giphy documentation to provide proper html request // 
+
