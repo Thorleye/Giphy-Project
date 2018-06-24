74e0b8025899db378071ba4e148cb990639f9008
Thorley <thorley.eric@gmail.com>
2018-06-24 16:19:28 -0400

    fun button times

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,40 @@
+
+
 //Initial array//
 
-var gifArray = ["Fifa", "NHL", "NBA", "NFL"]
-console.log(gifArray)
+var gifArray = ["Fifa-bicycle-kick", "NHL-save", "NHL-hit", "NBA-block", "NBA-windmill", "NBA-half-court",
+"NFL-celebration", "NFL-sack", "NFL-touchdown", "NHL-breakaway"]
 //creating the buttons based on the array//  
 
-function gifButtons(){
+    function makeButtons(){
           
     // Delete the content inside the movies-view div prior to adding new movies
     $("#gif-button-area").empty()
   
     for (i=0; i < gifArray.length; i++){
     var gifButton = $("<button>")
-   gifButton.text(gifArray[i])
-   gifButton.addClass("btn btn-primary")
-    $("#gif-button-area").append(gifButton)
+    gifButton.text(gifArray[i]);
+    gifButton.addClass("btn btn-primary");
+    gifButton.attr("data-name", gifArray[i]);
+    $("#gif-button-area").append(gifButton);
     }
 }
+
 // add the button based on the array on the search //
+    function searchGif(){
+    $("#searchButton").on("click", function(event){
+    event.preventDefault()
+    var newGif = $("#gif-search").val().trim()
+    gifArray.push(newGif)
+    makeButtons()
+    })
+}
+
+makeButtons()
+searchGif()
+
 
 // the AJAX call //
 
 // giphy documentation to provide proper html request // 
 
-gifButtons()
